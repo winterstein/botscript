035ad3e747d99b7cf884cc53d113e44de2fa8bae
Daniel Winterstein <daniel@sodash.com>
2017-02-09 21:44:44 +0000

    notes

--- src/Scriptlet.js
+++ src/Scriptlet.js
@@ -15,3 +15,15 @@ onBlur (losing focus to a non-child/descendant)
 onOutput (ie your output has been emitted)
 
 Only output: another scriptlet
+
+parents do not get called unless the child explicitly does with 
+ -- except for done, fail
+
+actions:
+start()
+done()
+fail()
+input()
+partialInput()
+focus()
+blur() -> release focus, the runner then decides who to give focus to
